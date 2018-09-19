4cf66d5e0885c77c8d5a8f373fae4a8d6e275ac3
大锅 <595554725@qq.com>
2018-09-19 20:39:36 +0800

    '加油'

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,9 @@
   <div id="app">
     <img src="./assets/logo.png">
     <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
       <el-button>el-button</el-button>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="我是大锅"/>
   </div>
 </template>
 
---
+++ src/views/GoodsList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    商品列表
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GoodsList',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
